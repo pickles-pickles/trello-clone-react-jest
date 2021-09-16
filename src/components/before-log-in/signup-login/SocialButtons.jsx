886bdcd40fbff5379521838ded86d625365257b1
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2021-09-16 18:50:47 +0300

    invalid DOM names fixed

--- src/components/before-log-in/signup-login/SocialButtons.jsx
+++ src/components/before-log-in/signup-login/SocialButtons.jsx
@@ -6,7 +6,7 @@ const SocialButtons = () => {
   return (
     <>
       {appState.icons.map(icon => (
-        <div className='w-100'>
+        <div className='w-100' key={icon.social}>
           <form>
             <button className='btn btn-block d-flex align-items-center justify-content-center my-1 shadow icons-btn'>
               <img src={icon.url} alt='' width='20px' />
